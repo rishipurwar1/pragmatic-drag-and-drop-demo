dc6b47140b7f9f2fabef030f0d20739c011b7d2b
rishipurwar1 <rishipurwar007@gmail.com>
2024-06-08 13:52:40 +0530

    starter code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pragmatic drag and drop demo in ReactJs</h1>
+      <p>
+        Flexible and fast drag and drop for any experience on any tech stack
+      </p>
     </div>
   );
 }
