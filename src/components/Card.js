f92f377494aa46e7339cd9866af6906aef375cc9
rishipurwar1 <rishipurwar007@gmail.com>
2024-06-17 16:43:11 +0530

    make card draggable

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,38 @@
+import { useEffect, useRef, useState } from "react";
+import invariant from "tiny-invariant";
+import {
+  draggable,
+  dropTargetForElements,
+} from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
+import { combine } from "@atlaskit/pragmatic-drag-and-drop/combine";
+
 const Card = ({ children, ...card }) => {
-  return <div className="card">{children}</div>;
+  const cardRef = useRef(null);
+  const [isDragging, setIsDragging] = useState(false); // create a state for dragging
+
+  useEffect(() => {
+    const cardEl = cardRef.current;
+    invariant(cardEl);
+
+    return combine(
+      draggable({
+        element: cardEl,
+        onDragStart: () => setIsDragging(true), // set isDragging to true when dragging starts
+        onDrop: () => setIsDragging(false), // set isDragging to false when dragging ends
+        getInitialData: () => ({ type: "card", cardId: card.id }),
+      }),
+      dropTargetForElements({
+        element: cardEl,
+        getData: () => ({ type: "card", cardId: card.id }),
+      })
+    );
+  }, [card.id]);
+  return (
+    // Add dragging class when isDragging is true
+    <div className={`card ${isDragging ? "dragging" : ""}`} ref={cardRef}>
+      {children}
+    </div>
+  );
 };
 
 export default Card;
