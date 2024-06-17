30afca641ac97682bc8e964b04272411d29a21e2
rishipurwar1 <rishipurwar007@gmail.com>
2024-06-17 16:45:30 +0530

    update

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import invariant from "tiny-invariant";
-import {
-  draggable,
-  dropTargetForElements,
-} from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
+import { draggable } from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
 import { combine } from "@atlaskit/pragmatic-drag-and-drop/combine";
 
 const Card = ({ children, ...card }) => {
@@ -19,14 +16,9 @@ const Card = ({ children, ...card }) => {
         element: cardEl,
         onDragStart: () => setIsDragging(true), // set isDragging to true when dragging starts
         onDrop: () => setIsDragging(false), // set isDragging to false when dragging ends
-        getInitialData: () => ({ type: "card", cardId: card.id }),
-      }),
-      dropTargetForElements({
-        element: cardEl,
-        getData: () => ({ type: "card", cardId: card.id }),
       })
     );
-  }, [card.id]);
+  }, []);
   return (
     // Add dragging class when isDragging is true
     <div className={`card ${isDragging ? "dragging" : ""}`} ref={cardRef}>
