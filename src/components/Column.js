1f70adf2fd06acd1938e7ed18b3b4d17334a6bf5
rishipurwar1 <rishipurwar007@gmail.com>
2024-06-22 12:02:25 +0530

    make column a drop target

--- src/components/Column.js
+++ src/components/Column.js
@@ -1,8 +1,31 @@
+import { useEffect, useRef, useState } from "react";
+import invariant from "tiny-invariant";
+import { dropTargetForElements } from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
+
 import Card from "./Card";
 
 const Column = ({ columnId, title, cards }) => {
+  const columnRef = useRef(null);
+  const [isDraggedOver, setIsDraggedOver] = useState(false);
+
+  useEffect(() => {
+    const columnEl = columnRef.current;
+    invariant(columnEl);
+
+    return dropTargetForElements({
+      element: columnEl,
+      onDragEnter: () => setIsDraggedOver(true),
+      onDragStart: () => setIsDraggedOver(true),
+      onDragLeave: () => setIsDraggedOver(false),
+      onDrop: () => setIsDraggedOver(false),
+      getData: () => ({ columnId }),
+    });
+  }, [columnId]);
   return (
-    <div className="column">
+    <div
+      className={`column ${isDraggedOver ? "dragged-over" : ""}`}
+      ref={columnRef} // attach a columnRef to the column div
+    >
       <h2>{title}</h2>
       {cards.map((card) => (
         <Card key={card.id} {...card}>
